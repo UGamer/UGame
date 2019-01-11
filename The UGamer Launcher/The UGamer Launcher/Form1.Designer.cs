d3f18e531cf2b030eba88cdd3929397a53b59482
UGamer <itsugamer@gmail.com>
2019-01-11 18:28:03 -0500

    Release 1.0.0
    The first release of my personal launcher! It only took two days to make the launcher itself, however the website that the launcher relies on has been in development for about a week now.
    
    + Added launcher functionality.
    
    Things To Do
    ==============================
    + Complete the collection website.
    + Add Search
    + Add Filters
    + Add Categories

--- The UGamer Launcher/The UGamer Launcher/Form1.Designer.cs
+++ The UGamer Launcher/The UGamer Launcher/Form1.Designer.cs
@@ -29,80 +29,96 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.launcherButton = new System.Windows.Forms.Button();
+            this.collectionButton = new System.Windows.Forms.Button();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // launcherButton
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.FlatAppearance.BorderSize = 2;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(82, 195);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(113, 64);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Launcher";
-            this.button1.UseVisualStyleBackColor = false;
+            this.launcherButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.launcherButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.launcherButton.FlatAppearance.BorderSize = 2;
+            this.launcherButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.launcherButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.launcherButton.Location = new System.Drawing.Point(1182, 533);
+            this.launcherButton.Margin = new System.Windows.Forms.Padding(2);
+            this.launcherButton.Name = "launcherButton";
+            this.launcherButton.Size = new System.Drawing.Size(113, 64);
+            this.launcherButton.TabIndex = 0;
+            this.launcherButton.Text = "Launcher";
+            this.launcherButton.UseVisualStyleBackColor = false;
+            this.launcherButton.Click += new System.EventHandler(this.launcherButto);
             // 
-            // button2
+            // collectionButton
             // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button2.FlatAppearance.BorderSize = 2;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.SystemColors.Control;
-            this.button2.Location = new System.Drawing.Point(340, 195);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(113, 64);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Collection";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.collectionButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.collectionButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.collectionButton.FlatAppearance.BorderSize = 2;
+            this.collectionButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.collectionButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.collectionButton.Location = new System.Drawing.Point(1182, 533);
+            this.collectionButton.Margin = new System.Windows.Forms.Padding(2);
+            this.collectionButton.Name = "collectionButton";
+            this.collectionButton.Size = new System.Drawing.Size(113, 64);
+            this.collectionButton.TabIndex = 1;
+            this.collectionButton.Text = "Collection";
+            this.collectionButton.UseVisualStyleBackColor = false;
+            this.collectionButton.Click += new System.EventHandler(this.collectionButto);
             // 
-            // pictureBox1
+            // logo
             // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(111, 40);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(303, 108);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.logo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.logo.BackColor = System.Drawing.Color.Transparent;
+            this.logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("logo.BackgroundImage")));
+            this.logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.logo.Location = new System.Drawing.Point(12, 533);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(188, 70);
+            this.logo.TabIndex = 2;
+            this.logo.TabStop = false;
+            this.logo.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(12, 12);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1282, 515);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("https://ugamer.github.io/Library/launcher/launcher.html", System.UriKind.Absolute);
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
             // 
             // Form1
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.AutoSize = true;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(533, 292);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(1306, 615);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.collectionButton);
+            this.Controls.Add(this.launcherButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "UGame Launcher";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button launcherButton;
+        private System.Windows.Forms.Button collectionButton;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
--- The UGamer Launcher/The UGamer Launcher/Form1.cs
+++ The UGamer Launcher/The UGamer Launcher/Form1.cs
@@ -15,15 +15,41 @@ namespace The_UGamer_Launcher
         public Form1()
         {
             InitializeComponent();
+            Uri url = new Uri("https://ugamer.github.io/Library/launcher/launcher.html");
+            if (webBrowser1.Url == url)
+            {
+                launcherButton.Visible = true;
+                collectionButton.Visible = false;
+            }
+            else
+            {
+                collectionButton.Visible = true;
+                launcherButton.Visible = false;
+            }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void collectionButto(object sender, EventArgs e)
         {
-            CollectionForm collection = new CollectionForm();
-            collection.Show();
+            Uri url = new Uri("https://ugamer.github.io/Library/collection.html");
+            webBrowser1.Url = url;
+            launcherButton.Visible = true;
+            collectionButton.Visible = false;
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void launcherButto(object sender, EventArgs e)
+        {
+            Uri url = new Uri("https://ugamer.github.io/Library/launcher/launcher.html");
+            webBrowser1.Url = url;
+            collectionButton.Visible = true;
+            launcherButton.Visible = false;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
 
         }
