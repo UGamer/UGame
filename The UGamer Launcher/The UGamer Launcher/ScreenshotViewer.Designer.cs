dabafae95f35341dd50ff9daa03ce3cd427fb4cc
UGamer <itsugamer@gmail.com>
2019-03-07 13:49:01 -0500

    [BETA] 5.4.2
    Screenshot viewer almost done. Add maximize.
    
    + You can switch pictures now.
    + Pictures are now displayed.
    + Switching pictures changes labels.

--- The UGamer Launcher/The UGamer Launcher/ScreenshotViewer.Designer.cs
+++ The UGamer Launcher/The UGamer Launcher/ScreenshotViewer.Designer.cs
@@ -31,20 +31,14 @@
             this.ChooseGameCombo = new System.Windows.Forms.ComboBox();
             this.FocusedPictureBox = new System.Windows.Forms.PictureBox();
             this.DetailsPanel = new System.Windows.Forms.Panel();
-            this.DetailLabels = new System.Windows.Forms.Label();
-            this.FileNameLabel = new System.Windows.Forms.Label();
-            this.GameNameLabel = new System.Windows.Forms.Label();
             this.TimeTakenLabel = new System.Windows.Forms.Label();
+            this.GameNameLabel = new System.Windows.Forms.Label();
+            this.FileNameLabel = new System.Windows.Forms.Label();
+            this.DetailLabels = new System.Windows.Forms.Label();
             this.ImagesPanel = new System.Windows.Forms.Panel();
             this.NoScreenshotsLabel = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.FocusedPictureBox)).BeginInit();
             this.DetailsPanel.SuspendLayout();
-            this.ImagesPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // ChooseGameCombo
@@ -53,9 +47,10 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.ChooseGameCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.ChooseGameCombo.FormattingEnabled = true;
-            this.ChooseGameCombo.Location = new System.Drawing.Point(13, 14);
+            this.ChooseGameCombo.Location = new System.Drawing.Point(20, 22);
+            this.ChooseGameCombo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ChooseGameCombo.Name = "ChooseGameCombo";
-            this.ChooseGameCombo.Size = new System.Drawing.Size(775, 21);
+            this.ChooseGameCombo.Size = new System.Drawing.Size(1160, 28);
             this.ChooseGameCombo.TabIndex = 0;
             // 
             // FocusedPictureBox
@@ -64,9 +59,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.FocusedPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.FocusedPictureBox.Location = new System.Drawing.Point(13, 41);
+            this.FocusedPictureBox.Location = new System.Drawing.Point(20, 63);
+            this.FocusedPictureBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.FocusedPictureBox.Name = "FocusedPictureBox";
-            this.FocusedPictureBox.Size = new System.Drawing.Size(538, 297);
+            this.FocusedPictureBox.Size = new System.Drawing.Size(807, 464);
             this.FocusedPictureBox.TabIndex = 1;
             this.FocusedPictureBox.TabStop = false;
             // 
@@ -78,63 +74,69 @@
             this.DetailsPanel.Controls.Add(this.GameNameLabel);
             this.DetailsPanel.Controls.Add(this.FileNameLabel);
             this.DetailsPanel.Controls.Add(this.DetailLabels);
-            this.DetailsPanel.Location = new System.Drawing.Point(558, 41);
+            this.DetailsPanel.Location = new System.Drawing.Point(837, 63);
+            this.DetailsPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.DetailsPanel.Name = "DetailsPanel";
-            this.DetailsPanel.Size = new System.Drawing.Size(230, 297);
+            this.DetailsPanel.Size = new System.Drawing.Size(345, 464);
             this.DetailsPanel.TabIndex = 2;
-            // 
-            // DetailLabels
-            // 
-            this.DetailLabels.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.DetailLabels.AutoSize = true;
-            this.DetailLabels.Location = new System.Drawing.Point(3, 29);
-            this.DetailLabels.Name = "DetailLabels";
-            this.DetailLabels.Size = new System.Drawing.Size(70, 234);
-            this.DetailLabels.TabIndex = 0;
-            this.DetailLabels.Text = "File Name: \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nGame: \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nTime Taken: ";
-            // 
-            // FileNameLabel
-            // 
-            this.FileNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.FileNameLabel.AutoSize = true;
-            this.FileNameLabel.Location = new System.Drawing.Point(80, 29);
-            this.FileNameLabel.Name = "FileNameLabel";
-            this.FileNameLabel.Size = new System.Drawing.Size(35, 13);
-            this.FileNameLabel.TabIndex = 1;
-            this.FileNameLabel.Text = "label1";
-            // 
-            // GameNameLabel
-            // 
-            this.GameNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.GameNameLabel.AutoSize = true;
-            this.GameNameLabel.Location = new System.Drawing.Point(80, 147);
-            this.GameNameLabel.Name = "GameNameLabel";
-            this.GameNameLabel.Size = new System.Drawing.Size(35, 13);
-            this.GameNameLabel.TabIndex = 2;
-            this.GameNameLabel.Text = "label1";
             // 
             // TimeTakenLabel
             // 
             this.TimeTakenLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.TimeTakenLabel.AutoSize = true;
-            this.TimeTakenLabel.Location = new System.Drawing.Point(80, 250);
+            this.TimeTakenLabel.Location = new System.Drawing.Point(99, 385);
+            this.TimeTakenLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TimeTakenLabel.Name = "TimeTakenLabel";
-            this.TimeTakenLabel.Size = new System.Drawing.Size(35, 13);
+            this.TimeTakenLabel.Size = new System.Drawing.Size(51, 20);
             this.TimeTakenLabel.TabIndex = 3;
             this.TimeTakenLabel.Text = "label1";
+            // 
+            // GameNameLabel
+            // 
+            this.GameNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GameNameLabel.AutoSize = true;
+            this.GameNameLabel.Location = new System.Drawing.Point(61, 225);
+            this.GameNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.GameNameLabel.Name = "GameNameLabel";
+            this.GameNameLabel.Size = new System.Drawing.Size(51, 20);
+            this.GameNameLabel.TabIndex = 2;
+            this.GameNameLabel.Text = "label1";
+            // 
+            // FileNameLabel
+            // 
+            this.FileNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileNameLabel.AutoSize = true;
+            this.FileNameLabel.Location = new System.Drawing.Point(88, 45);
+            this.FileNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.FileNameLabel.Name = "FileNameLabel";
+            this.FileNameLabel.Size = new System.Drawing.Size(51, 20);
+            this.FileNameLabel.TabIndex = 1;
+            this.FileNameLabel.Text = "label1";
+            // 
+            // DetailLabels
+            // 
+            this.DetailLabels.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DetailLabels.AutoSize = true;
+            this.DetailLabels.Location = new System.Drawing.Point(4, 45);
+            this.DetailLabels.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.DetailLabels.Name = "DetailLabels";
+            this.DetailLabels.Size = new System.Drawing.Size(99, 360);
+            this.DetailLabels.TabIndex = 0;
+            this.DetailLabels.Text = "File Name: \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nGame: \r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nTime Taken: ";
             // 
             // ImagesPanel
             // 
             this.ImagesPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.ImagesPanel.Controls.Add(this.panel1);
-            this.ImagesPanel.Location = new System.Drawing.Point(13, 345);
+            this.ImagesPanel.AutoScroll = true;
+            this.ImagesPanel.Location = new System.Drawing.Point(20, 537);
+            this.ImagesPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ImagesPanel.Name = "ImagesPanel";
-            this.ImagesPanel.Size = new System.Drawing.Size(775, 109);
+            this.ImagesPanel.Size = new System.Drawing.Size(1162, 288);
             this.ImagesPanel.TabIndex = 3;
             // 
             // NoScreenshotsLabel
@@ -144,58 +146,30 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.NoScreenshotsLabel.AutoSize = true;
             this.NoScreenshotsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NoScreenshotsLabel.Location = new System.Drawing.Point(97, 179);
+            this.NoScreenshotsLabel.Location = new System.Drawing.Point(146, 275);
+            this.NoScreenshotsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.NoScreenshotsLabel.Name = "NoScreenshotsLabel";
-            this.NoScreenshotsLabel.Size = new System.Drawing.Size(358, 25);
+            this.NoScreenshotsLabel.Size = new System.Drawing.Size(534, 37);
             this.NoScreenshotsLabel.TabIndex = 4;
             this.NoScreenshotsLabel.Text = "No screenshots found for this game!";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(129, 74);
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(3, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(135, 106);
-            this.panel1.TabIndex = 6;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 84);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
-            // 
             // ScreenshotViewer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 465);
+            this.ClientSize = new System.Drawing.Size(1200, 859);
             this.Controls.Add(this.NoScreenshotsLabel);
             this.Controls.Add(this.ImagesPanel);
             this.Controls.Add(this.DetailsPanel);
             this.Controls.Add(this.FocusedPictureBox);
             this.Controls.Add(this.ChooseGameCombo);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ScreenshotViewer";
             this.Text = "ScreenshotViewer";
             this.TopMost = true;
             ((System.ComponentModel.ISupportInitialize)(this.FocusedPictureBox)).EndInit();
             this.DetailsPanel.ResumeLayout(false);
             this.DetailsPanel.PerformLayout();
-            this.ImagesPanel.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -212,8 +186,5 @@
         private System.Windows.Forms.Label TimeTakenLabel;
         private System.Windows.Forms.Panel ImagesPanel;
         private System.Windows.Forms.Label NoScreenshotsLabel;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
     }
 }
--- The UGamer Launcher/The UGamer Launcher/ScreenshotViewer.cs
+++ The UGamer Launcher/The UGamer Launcher/ScreenshotViewer.cs
@@ -16,17 +16,12 @@ namespace The_UGamer_Launcher
         string titleFriendly;
 
         string[] files;
-        Image currentImage;
         Panel[] picturePanel;
         PictureBox[] pictureBox;
         Label[] pictureLabel;
 
         int xPosPanel = 3;
         int yPosPanel = 0;
-        int xPosBox = 3;
-        int yPosBox = 3;
-        int xPosLabel = 3;
-        int yPosLabel = 84;
         Point location;
 
         public ScreenshotViewer(string titleFriendly)
@@ -52,10 +47,38 @@ namespace The_UGamer_Launcher
                 pictureBox = new PictureBox[files.Length];
                 pictureLabel = new Label[files.Length];
 
+                Size picturePanelSize = new Size(202, 163);
+                Size pictureBoxSize = new Size(194, 114);
+                Size pictureLabelSize = new Size(51,20);
+
+                Point pictureBoxLocation = new Point(4, 5);
+                Point pictureLabelLocation = new Point(4, 129);
+
                 for (int index = 0; index < files.Length; index++)
                 {
+                    picturePanel[index] = new Panel();
+                    pictureBox[index] = new PictureBox();
+                    pictureLabel[index] = new Label();
+
+                    ImagesPanel.Controls.Add(picturePanel[index]);
+                    picturePanel[index].Controls.Add(pictureBox[index]);
+                    picturePanel[index].Controls.Add(pictureLabel[index]);
+
                     location = new Point(xPosPanel, yPosPanel);
-                    // picturePanel.Location = location;
+                    picturePanel[index].Location = location;
+                    picturePanel[index].Size = picturePanelSize;
+                    xPosPanel += 210;
+
+                    pictureBox[index].Location = pictureBoxLocation;
+                    pictureBox[index].Size = pictureBoxSize;
+                    pictureBox[index].BackgroundImage = Image.FromFile(files[index]);
+                    pictureBox[index].BackgroundImageLayout = ImageLayout.Zoom;
+                    pictureBox[index].Tag = index;
+                    pictureBox[index].Click += new EventHandler(pictureBox_Click);
+
+                    pictureLabel[index].Location = pictureLabelLocation;
+                    pictureLabel[index].Size = pictureLabelSize;
+                    pictureLabel[index].Text = files[index];
                 }
             }
         }
@@ -63,6 +86,52 @@ namespace The_UGamer_Launcher
         private void InitializeDesign()
         {
 
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            PictureBox tempPic = (PictureBox)sender;
+            string tagString = tempPic.Tag.ToString();
+            int index = Convert.ToInt32(tagString);
+            FocusedPictureBox.BackgroundImage = Image.FromFile(files[index]);
+
+            int gameIndex = files[index].IndexOf("Screenshots\\");
+
+            string fileName = files[index];
+            string gameName = files[index];
+
+            gameName = gameName.Substring(gameIndex + 12);
+
+            int gameLength = gameName.IndexOf("\\");
+            gameName = gameName.Substring(0, gameLength);
+            
+            fileName = fileName.Substring(gameIndex + gameName.Length + 13);
+            string type = ".jpg";
+            int getRidOfExtension = fileName.IndexOf(".jpg");
+            
+            if (getRidOfExtension == -1)
+            {
+                getRidOfExtension = fileName.IndexOf(".png");
+                    type = ".png";
+            }
+                
+            if (getRidOfExtension == -1)
+                getRidOfExtension = fileName.IndexOf(".jpeg");
+            if (getRidOfExtension == -1)
+                getRidOfExtension = fileName.IndexOf(".gif");
+
+            string extension = fileName;
+            int length = fileName.Length;
+            int extensionIndex = extension.IndexOf(type);
+            extension = extension.Substring(0, type.Length);
+            length -= extension.Length;
+            fileName = fileName.Substring(0, length);
+
+            string timeTaken = File.GetCreationTime(files[index]).ToString();
+
+            FileNameLabel.Text = fileName;
+            GameNameLabel.Text = gameName;
+            TimeTakenLabel.Text = timeTaken;
         }
     }
 }
